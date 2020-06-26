f7367df456b49838cd107b08de7bfcf81f8fccf0
jorigeysen <jori.geysen@anmut.co.uk>
2020-06-26 18:22:16 +0200

    add parse_short_sentences functionality

--- tests/test_sentence_splitting.py
+++ tests/test_sentence_splitting.py
@@ -52,39 +52,39 @@ def test_parse_short_sentences() -> None:
         columns=["sentences", "identifier"],
     )
 
-    # output1_df = pd.DataFrame(
-    #     list(
-    #         zip(
-    #             [
-    #                 "First sentence of the first article.",
-    #                 "Short sentence. This is a much longer intermediate sentence of the article, rather short sentences before and after should be parsed to this one.",
-    #                 "Final one.",
-    #                 "First sentence of a second article.",
-    #                 "Short sentence. Another short one.",
-    #                 "A long sentence, short sentences should be parsed to this one.",
-    #                 "Final sentence of the second article.",
-    #                 "First sentence of a third article, which only has one sentence.",
-    #             ],
-    #             [
-    #                 "article_id1",
-    #                 "article_id1",
-    #                 "article_id1",
-    #                 "article_id2",
-    #                 "article_id2",
-    #                 "article_id2",
-    #                 "article_id2",
-    #                 "article_id3",
-    #             ],
-    #         )
-    #     ),
-    #     columns=["sentences", "identifier"],
-    # )
-    # min_length = 3
-    # combination = "next"
-    # assert np.array_equal(
-    #     parse_short_sentences(input1_df, min_length, combination).sentences.values,
-    #     output1_df.sentences.values,
-    # )
+    output1_df = pd.DataFrame(
+        list(
+            zip(
+                [
+                    " First sentence of the first article.",
+                    "Short sentence. This is a much longer intermediate sentence of the article, rather short sentences before and after should be parsed to this one.",
+                    " Final one.",
+                    " First sentence of a second article.",
+                    "Short sentence. Another short one.",
+                    " A long sentence, short sentences should be parsed to this one.",
+                    " Final sentence of the second article.",
+                    " First sentence of a third article, which only has one sentence.",
+                ],
+                [
+                    "article_id1",
+                    "article_id1",
+                    "article_id1",
+                    "article_id2",
+                    "article_id2",
+                    "article_id2",
+                    "article_id2",
+                    "article_id3",
+                ],
+            )
+        ),
+        columns=["sentences", "identifier"],
+    )
+    min_length = 3
+    combination = "next"
+    assert np.array_equal(
+        parse_short_sentences(input1_df, min_length, combination).sentences.values,
+        output1_df.sentences.values,
+    )
     min_length = 3
     combination = "previous"
     output2_df = pd.DataFrame(
