0ec51a680bc260487b4e35a4b59c4fefb294f44f
jorigeysen <jori.geysen@anmut.co.uk>
2020-07-02 02:46:23 +0200

    add regex preprocessing for feature engineering

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -1,2 +1,15 @@
-def test_preprocess_regexes() -> None:
-    assert True
+from relevance.feature_engineering import preprocess_regex
+
+
+def test_preprocess_regex() -> None:
+    entity_list = ["Aviva"]
+    regex_dict = {"Aviva": {"alias": ["Aviva"], "abbrev": ["Aviva"]}}
+    all_regex_list = [
+        "aviva",
+        "[^a-zA-Z]+aviva[^a-zA-Z]+",
+        "^aviva[^a-zA-Z]+",
+        "[^a-zA-Z]+aviva$",
+        "^aviva$",
+    ]
+    # all_regex_dict = {'alias': ['aviva'], 'abbrev': ['[^a-zA-Z]+aviva[^a-zA-Z]+', '^aviva[^a-zA-Z]+', '[^a-zA-Z]+aviva$', '^aviva$']}
+    assert preprocess_regex(entity_list, regex_dict) == all_regex_list
